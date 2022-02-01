4ec7b969d302f4800ea1288bb5c63ed75cae7b3c
Liam Howley <liam.howley@hotmail.com>
2022-02-01 00:33:26 +0000

    p-values slot to store array args, call procedure with format string

--- procedure.lisp
+++ procedure.lisp
@@ -5,9 +5,9 @@
   :in db-layer ()
   ((name :initarg :name :accessor name)
    (args :initarg :args :initform nil :accessor args)
+   (p-values :initarg :values :initform nil :accessor p-values)
    (vars :initarg :vars :initform nil :accessor vars)
    (statements :initarg :statements :accessor statements)))
-
 
 (define-layered-method statement
   :in-layer db-interface-layer ((class procedure))
@@ -17,3 +17,11 @@
 	    (format nil "~@[~{~{~a~@[ ~a~]~}~^, ~}~]" args)
 	    (format nil "~@[~{~{DECLARE ~a ~a~@[ := ~a~];~}~%~}~]" vars)
 	    (format nil "~{~a~%~}" statements))))
+
+(define-layered-function call (procedure)
+  (:method
+      :in db-layer ((class procedure))
+    (with-slots (name p-values) class
+      (format nil "CALL ~a (~@[~{~a~^, ~}~])" name p-values))))
+
+
