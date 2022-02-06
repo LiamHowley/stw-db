679ff075d0d0b580a66b3393a9fe42bbf6ecac40
Liam Howley <liam.howley@hotmail.com>
2022-02-06 22:38:29 +0000

    minor formatting changes to statement method for building procedures; a method for building composite arrays for insertion

--- procedure.lisp
+++ procedure.lisp
@@ -7,21 +7,37 @@
    (args :initarg :args :initform nil :accessor args)
    (p-values :initarg :values :initform nil :accessor p-values)
    (vars :initarg :vars :initform nil :accessor vars)
-   (statements :initarg :statements :accessor statements)))
+   (sql-list :initarg :sql-list :initform nil :accessor sql-list)
+   (sql-statement :initarg :sql-statement :initform nil :reader sql-statement)))
+
 
 (define-layered-method statement
   :in-layer db-interface-layer ((class procedure))
-  (with-slots (name args vars statements) class
-    (format nil "CREATE OR REPLACE PROCEDURE ~a (~a)~%LANGUAGE plpgsql~%AS $BODY$~%~a~%BEGIN~%~a~%END;~%$BODY$;"
-	    name 
-	    (format nil "~@[~{~{~a~@[ ~a~]~}~^, ~}~]" args)
-	    (format nil "~@[~{~{DECLARE ~a ~a~@[ := ~a~];~}~%~}~]" vars)
-	    (format nil "~{~a~%~}" statements))))
+  (with-slots (name args vars sql-list sql-statement) class
+    (setf sql-statement
+	  (format nil "CREATE OR REPLACE PROCEDURE ~a (~a)~%LANGUAGE plpgsql~%AS $BODY$~%~a~%BEGIN~%~a~%END;~%$BODY$;"
+		  name 
+		  (format nil "~@[~{~{~a~@[ ~a~]~}~^, ~}~]" args)
+		  (format nil "~@[~%~{~{DECLARE ~a ~a~@[ := ~a~];~}~%~}~]" vars)
+		  (format nil "~{~a~^~%~}" sql-list)))
+    class))
 
-(define-layered-function call (procedure)
+;;; pg composite arrays - used in passing values to insert procedure calls
+
+(define-layered-function sql-typed-array (class)
   (:method
-      :in db-layer ((class procedure))
-    (with-slots (name p-values) class
-      (format nil "CALL ~a (~@[~{~a~^, ~}~])" name p-values))))
+      :in db-layer ((class db-table-class))
+    (with-slots (schema table require-columns) class
+      (let ((table-name (set-sql-name schema table))
+	    (slots))
+	(list 
+	 (format nil "ARRAY[(~{~a~^, ~})]::~a_type"
+		 (loop for slot in require-columns
+		       collect slot into slots%
+		       collect "~a" into values
+		       finally (setf slots slots%)
+			       (return values))
+		 table-name)
+	 slots)))))
 
-
+	  ;;(format nil "ARRAY[~{(~{~a~^, ~})~^, ~}]::~a_type"
