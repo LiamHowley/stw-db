306d73979c5056731f876619506d0d51e55c053c
Liam Howley <liam.howley@hotmail.com>
2025-12-29 22:13:27 +0000

    indenting file

--- setting-up.lisp
+++ setting-up.lisp
@@ -36,23 +36,23 @@
       for table in (slot-value class 'tables)
       for object = (find-class table)
       for statement = (with-active-layers (db-table-layer)
-			(create-table-statement object))
+			                  (create-table-statement object))
       when statement
-	collect statement))
+	      collect statement))
 
   (:method
       :in db-table-layer ((class db-table-class))
     (with-slots (schema table primary-keys constraints) class
       (princ (format nil "Creating table: ~s in schema: ~s~%" table schema))
       (format nil "CREATE TABLE IF NOT EXISTS ~a.~a (~{~a~^, ~}~@[, ~a~]~@[, ~{~a~^, ~}~]);"
-	      schema table
-	      (loop for column in (filter-slots-by-type class 'db-column-slot-definition)
-		    collect (clause column))
-	      ;; primary keys
-	      (clause (make-instance 'primary-key :keys primary-keys))
-	      ;;check constraints
-	      (loop for constraint in constraints
-		    collect (clause (apply #'make-instance 'check-constraint :table table constraint)))))))
+	            schema table
+	            (loop for column in (filter-slots-by-type class 'db-column-slot-definition)
+		                collect (clause column))
+	            ;; primary keys
+	            (clause (make-instance 'primary-key :keys primary-keys))
+	            ;;check constraints
+	            (loop for constraint in constraints
+		                collect (clause (apply #'make-instance 'check-constraint :table table constraint)))))))
 
 
 (define-layered-class primary-key
@@ -71,33 +71,33 @@
 (defmethod initialize-instance :after ((this check-constraint) &key)
   (with-slots (check col-name table constraint-name) this
     (setf check (infix-constraint check col-name)
-	  constraint-name (format nil "~a_~a_check" (db-syntax-prep table) (db-syntax-prep col-name)))))
+	        constraint-name (format nil "~a_~a_check" (db-syntax-prep table) (db-syntax-prep col-name)))))
 
 
 (define-layered-method clause
   :in-layer db-table-layer ((column db-column-slot-definition))
   (with-slots (column-name col-type not-null unique) column
     (format nil "~(~a~) ~{~a~}"
-	    column-name
-      (list
-       (format nil "~a" col-type)
-       (if not-null " NOT NULL" "")
-       (if (eq unique t) " UNIQUE" "")
-       (if (slot-boundp column 'default)
-           (let ((default (slot-value column 'default)))
-             (typecase default
-               (cons
-                (format nil " DEFAULT ~a(~@[~{~a~^, ~}~])" (car default) (cdr default)))
-               (integer
-                (format nil " DEFAULT ~a" default))
-               (string
-                (format nil " DEFAULT '~a'" default))
-               (boolean
-                (if (eq col-type :boolean)
-                    (format nil " DEFAULT '~a'" (if (eq default t) "t" "f"))
-                    ""))
-               (t "")))
-           "")))))
+	          column-name
+            (list
+             (format nil "~a" col-type)
+             (if not-null " NOT NULL" "")
+             (if (eq unique t) " UNIQUE" "")
+             (if (slot-boundp column 'default)
+                 (let ((default (slot-value column 'default)))
+                   (typecase default
+                     (cons
+                      (format nil " DEFAULT ~a(~@[~{~a~^, ~}~])" (car default) (cdr default)))
+                     (integer
+                      (format nil " DEFAULT ~a" default))
+                     (string
+                      (format nil " DEFAULT '~a'" default))
+                     (boolean
+                      (if (eq col-type :boolean)
+                          (format nil " DEFAULT '~a'" (if (eq default t) "t" "f"))
+                          ""))
+                     (t "")))
+                 "")))))
 
 
 (define-layered-method clause
@@ -123,28 +123,28 @@
       :in db-interface-layer ((class db-interface-class))
     (with-slots (tables) class
       (let (collated-keys)
-	(loop for table in tables
-	      for f-keys = (with-active-layers (db-table-layer)
-			     (foreign-keys-statements (find-class table)))
-	      when f-keys
-		do (setf collated-keys (nconc collated-keys f-keys)))
-	collated-keys)))
+	      (loop for table in tables
+	            for f-keys = (with-active-layers (db-table-layer)
+			                       (foreign-keys-statements (find-class table)))
+	            when f-keys
+		            do (setf collated-keys (nconc collated-keys f-keys)))
+	      collated-keys)))
 
   (:method
       :in db-table-layer ((class db-table-class))
     (with-slots (schema table foreign-keys) class
       (loop for key in foreign-keys
-	    collect (statement key)))))
+	          collect (statement key)))))
 
 
 (define-layered-method statement
   :in-layer db-table-layer ((statement foreign-key))
   (with-slots (schema key ref-schema ref-table table column on-update on-delete) statement
     (let* ((root-key (db-syntax-prep key))
-	   (table-name (set-sql-name schema table))
-	   (referring-table (set-sql-name ref-schema ref-table))
-	   (constraint (format nil "~a_~a_~a_fkey" schema (db-syntax-prep ref-table) root-key))
-	   (column-name (db-syntax-prep column)))
+	         (table-name (set-sql-name schema table))
+	         (referring-table (set-sql-name ref-schema ref-table))
+	         (constraint (format nil "~a_~a_~a_fkey" schema (db-syntax-prep ref-table) root-key))
+	         (column-name (db-syntax-prep column)))
       (format nil "IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = '~a') THEN ALTER TABLE ~a ADD CONSTRAINT ~a FOREIGN KEY (~a) REFERENCES ~a (~a)~@[ ON UPDATE ~a~]~@[ ON DELETE ~a~]; end if;" constraint referring-table constraint root-key table-name column-name on-update on-delete))))
 
 
@@ -161,37 +161,37 @@
 
 (define-layered-function index-statement (class)
 
-  (:method 
+  (:method
       :in-layer db-interface-layer ((class db-interface-class))
     (princ "indexing...")
     (labels ((to-index (tables acc)
-	       (if (null tables)
-		   acc
-		   (to-index
-		    (cdr tables)
-		    (let ((statements
-			    (with-active-layers (db-table-layer)
-			      (index-statement (find-class (car tables))))))
-		      (cond ((consp statements)
-			     (nconc statements acc))
-			    ((stringp statements)
-			     (cons statements acc))
-			    (t acc)))))))
+	             (if (null tables)
+		               acc
+		               (to-index
+		                (cdr tables)
+		                (let ((statements
+			                      (with-active-layers (db-table-layer)
+			                        (index-statement (find-class (car tables))))))
+		                  (cond ((consp statements)
+			                       (nconc statements acc))
+			                      ((stringp statements)
+			                       (cons statements acc))
+			                      (t acc)))))))
       (to-index (slot-value class 'tables) nil)))
 
 
-  (:method 
+  (:method
       :in-layer db-table-layer ((class db-table-class))
     (with-slots (schema table primary-keys) class
       (loop for slot in (filter-slots-by-type class 'db-column-slot-definition)
-	    for key = (db-syntax-prep (slot-definition-name slot))
-	    unless (member slot primary-keys :test #'eq)
-	     when (or (slot-value slot 'foreign-key)
-		       (slot-value slot 'index))
-		collect (clause (make-instance 'index :schema schema
-						      :table table
-						      :name (format nil "~a_~a_~a_idx" schema table key)
-						      :columns key))))))
+	          for key = (db-syntax-prep (slot-definition-name slot))
+	          unless (member slot primary-keys :test #'eq)
+	            when (or (slot-value slot 'foreign-key)
+		                   (slot-value slot 'index))
+		            collect (clause (make-instance 'index :schema schema
+						                                          :table table
+						                                          :name (format nil "~a_~a_~a_idx" schema table key)
+						                                          :columns key))))))
 
 
 (define-layered-method clause
@@ -204,7 +204,7 @@
 
 
 (define-layered-function create-pg-composite (class)
-  (:documentation "Creating an explicit type associated with a class, 
+  (:documentation "Creating an explicit type associated with a class,
 allows the values to be expressed as an array within the arglist of a
 procedure. It is a simple matter then of calling unnest, using a positional
 parameter to reference the array. This is particularly useful when adding
@@ -214,24 +214,24 @@ multiple records in a one-to-many relationship.")
       :in db-interface-layer ((class db-interface-class))
     (with-slots (tables) class
       (loop
-	for table in tables
-	for required = (require-columns (find-class table))
-	for composite = (when required
-			  (with-active-layers (db-table-layer)
-			    (create-pg-composite (find-class table))))
-	when composite
-	  collect composite)))
+	      for table in tables
+	      for required = (require-columns (find-class table))
+	      for composite = (when required
+			                    (with-active-layers (db-table-layer)
+			                      (create-pg-composite (find-class table))))
+	      when composite
+	        collect composite)))
 
   (:method
-      :in db-table-layer ((class db-table-class)) 
+      :in db-table-layer ((class db-table-class))
     (with-slots (schema table require-columns) class
       (princ (format nil "Creating composite type: ~a_type~%" (as-prefix table)))
       (let ((table-name (set-sql-name schema (as-prefix table))))
-	(format nil "IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '~a_type') THEN CREATE TYPE ~a_type AS (~{~{~a ~a~}~^, ~}); END IF;"
-		(as-prefix table) table-name (mapcar #'(lambda (column)
-							      (list (column-name column)
-								    (col-type column)))
-							  require-columns))))))
+	      (format nil "IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '~a_type') THEN CREATE TYPE ~a_type AS (~{~{~a ~a~}~^, ~}); END IF;"
+		            (as-prefix table) table-name (mapcar #'(lambda (column)
+							                                           (list (column-name column)
+								                                               (col-type column)))
+							                                       require-columns))))))
 
 
 ;;; create domain
@@ -245,25 +245,25 @@ so that differing columns of the same type can be applied to a procedure call.")
       :in db-interface-layer ((class db-interface-class))
     (with-slots (tables) class
       (loop
-	for table in tables
-	for domain = (with-active-layers (db-table-layer)
-		       (create-typed-domain (find-class table)))
-	when domain
-	collect domain)))
+	      for table in tables
+	      for domain = (with-active-layers (db-table-layer)
+		                   (create-typed-domain (find-class table)))
+	      when domain
+	      collect domain)))
 
   (:method
       :in db-table-layer ((class db-table-class))
     (with-slots (schema table require-columns) class
       (format nil "~{~a~}"
-	      (let ((columns (filter-slots-by-type class 'db-column-slot-definition)))
-		(loop
-		  for column in columns
-		  do (princ (format nil "Creating domain: ~s~%" (slot-value column 'domain)))
-		  collect (with-slots (domain col-type) column
-			    (format nil "IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '~a') THEN CREATE DOMAIN ~a.~a AS ~a; END IF;"
-				    domain schema domain (if (eq col-type :serial)
-							     :integer
-							     col-type)))))))))
+	            (let ((columns (filter-slots-by-type class 'db-column-slot-definition)))
+		            (loop
+		              for column in columns
+		              do (princ (format nil "Creating domain: ~s~%" (slot-value column 'domain)))
+		              collect (with-slots (domain col-type) column
+			                      (format nil "IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '~a') THEN CREATE DOMAIN ~a.~a AS ~a; END IF;"
+				                            domain schema domain (if (eq col-type :serial)
+							                                               :integer
+							                                               col-type)))))))))
 
 
 ;;; setting up
@@ -274,14 +274,14 @@ so that differing columns of the same type can be applied to a procedure call.")
   (:method
       :in db-layer (class (name string) &rest function-list)
     (let ((procedure (make-instance 'procedure
-				    :schema (slot-value class 'schema)
-				    :name name)))
+				                            :schema (slot-value class 'schema)
+				                            :name name)))
       (setf (slot-value procedure 'sql-list)
-	    (reduce #'(lambda (a b)
-			(nconc a b))
-		    (mapcan #'(lambda (fn)
-				(list (funcall fn class)))
-			    function-list)))
+	          (reduce #'(lambda (a b)
+			                  (nconc a b))
+		                (mapcan #'(lambda (fn)
+				                        (list (funcall fn class)))
+			                      function-list)))
       (set-control procedure)
       (values
        (sql-statement (statement procedure))
