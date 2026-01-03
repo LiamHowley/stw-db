0fd431a8993ad241820fdda1aa51c32b8ff84887
Liam Howley <liam.howley@hotmail.com>
2026-01-03 20:51:47 +0000

    indenting

--- procedure.lisp
+++ procedure.lisp
@@ -111,7 +111,7 @@ of class with updated values.")
 
   (:method
       :in-layer db-table-layer ((class serialize) (component db-table-class) &key)
-    (with-slots (schema table require-columns) component
+    (with-slots (schema table) component
       (let ((procedure (make-instance 'procedure
 				                              :schema schema
 				                              :table class))
